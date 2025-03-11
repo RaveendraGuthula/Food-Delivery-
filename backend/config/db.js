73d02b756ff5fec856832d523d03bde7b4b1d895
Guthula Naga Raveendra <122366955+RaveendraGuthula@users.noreply.github.com>
2025-03-12 02:47:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export default async function connectDB(){
 
-    await mongoose.connect("mongodb+srv://raveendrag777:Raveendr%40555@cluster0.9jw6jer.mongodb.net/Food_Del").then(()=>console.log("db"));
+    await mongoose.connect(`${process.env.BACKEND_MONGOOSE_URL}`).then(()=>console.log("db"));
 }
